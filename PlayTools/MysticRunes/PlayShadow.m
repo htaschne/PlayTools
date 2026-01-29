4c319b0336e435d529007a3380b10c0303a2fcf5
Agatha Schneider <agatha.schneider@outlook.com>
2026-01-29 16:43:36 -0300

    jailbreak message fix from nyathea

--- PlayTools/MysticRunes/PlayShadow.m
+++ PlayTools/MysticRunes/PlayShadow.m
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 #import <PlayTools/PlayTools-Swift.h>
+#import <UIKit/UIKit.h>
 
 __attribute__((visibility("hidden")))
 @interface PlayShadowLoader : NSObject
@@ -116,6 +117,25 @@ __attribute__((visibility("hidden")))
     return @{};
 }
 
+// Endfield UIAlertController hook - silently blocks all alerts
+- (void)pm_endfield_presentViewController:(UIViewController *)viewControllerToPresent
+                                 animated:(BOOL)flag
+                               completion:(void (^)(void))completion {
+    // If it's a UIAlertController, silently ignore it
+    if ([viewControllerToPresent isKindOfClass:[UIAlertController class]]) {
+        NSLog(@"PC-DEBUG: [PlayShadow] Blocked UIAlertController for Endfield");
+        // Call completion handler if provided so the app doesn't hang
+        if (completion) {
+            completion();
+        }
+        return;
+    }
+
+    // Otherwise, present normally
+    [self pm_endfield_presentViewController:viewControllerToPresent animated:flag completion:completion];
+}
+
+
 // Class methods
 
 + (void) pm_return_2_with_completion_handler:(void (^)(NSInteger))completionHandler {
@@ -171,6 +191,13 @@ __attribute__((visibility("hidden")))
 
     // canResizeToFitContent
     // [objc_getClass("UIWindow") swizzleInstanceMethod:@selector(canResizeToFitContent) withMethod:@selector(pm_return_true)];
+
+    // Endfield: Block UIAlertController presentation for jailbreak bypass
+        NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
+        if ([bundleID isEqualToString:@"com.gryphline.endfield.ios"]) {
+            [self debugLogger:@"Endfield detected, loading UIAlertController bypass"];
+            [objc_getClass("UIViewController") swizzleInstanceMethod:@selector(presentViewController:animated:completion:) withMethod:@selector(pm_endfield_presentViewController:animated:completion:)];
+        }
 }
 
 + (void) loadJailbreakBypass {
